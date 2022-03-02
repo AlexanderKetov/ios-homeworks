1aad1ae0cf35e479da82943570b72044b7dfc68e
AlexanderKetov <alexket@inbox.ru>
2022-03-02 19:38:46 +0700

    q1

--- Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -14,21 +14,8 @@ class ProfileViewController: UIViewController {
 
         if let myView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
             myView.frame = CGRect(x: 10, y: 10, width: myView.frame.width-20, height: myView.frame.width-20)
-            
             view.addSubview(myView)
-            
         }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
