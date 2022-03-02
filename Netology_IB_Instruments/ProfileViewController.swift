d46474bc550b6c199d33e9791059fc674bff5eef
AlexanderKetov <alexket@inbox.ru>
2022-03-02 19:41:20 +0700

    q

--- Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -15,6 +15,7 @@ class ProfileViewController: UIViewController {
         if let myView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
             myView.frame = CGRect(x: 10, y: 10, width: myView.frame.width-20, height: myView.frame.width-20)
             view.addSubview(myView)
+            
         }
     }
 }
